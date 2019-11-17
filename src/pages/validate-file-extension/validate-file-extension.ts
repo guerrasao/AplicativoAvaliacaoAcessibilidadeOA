849adf626965b92e7569be24bce54788b1f41fb3
Patrick Guerra <patryckguerra@yahoo.com.br>
2019-11-17 13:09:07 -0300

    Update API Server Parameters
    Update request server

--- src/pages/validate-file-extension/validate-file-extension.ts
+++ src/pages/validate-file-extension/validate-file-extension.ts
@@ -14,6 +14,7 @@ import {MidiaOaDaoProvider} from "../../providers/dao/midia-oa-dao";
 import {DiretrizDaoProvider} from "../../providers/dao/diretriz-dao";
 import {DiretrizDeficienciaDaoProvider} from "../../providers/dao/diretriz-deficiencia-dao";
 import {DeficienciaDaoProvider} from "../../providers/dao/deficiencia-dao";
+import {APIServer} from "../../providers/se/APIServer";
 
 /**
  * Generated class for the ValidateFileExtensionPage page.
@@ -40,6 +41,7 @@ export class ValidateFileExtensionPage {
     private errorDisplay : ErrorDisplayProvider,
     private seProvider : SeProvider,
     public generalDAO : GeneralDaoProvider,
+    public apiServer : APIServer,
     public oaDAO : OaDaoProvider,
     public regraAtributoDAO : RegraAtributoDaoProvider,
     public atributoDAO : AtributoDaoProvider,
@@ -77,7 +79,7 @@ export class ValidateFileExtensionPage {
 
     let nav : NavigationProvider = new NavigationProvider();
 
-    this.http.uploadFile('http://192.168.0.111:3001/file/upload', {}, {}, this.hasFile.uri, 'file' ).then( resp => {
+    this.http.uploadFile(this.apiServer.url, {}, {}, this.hasFile.uri, 'file' ).then( resp => {
       //console.log(resp);
       loadingEnv.dismiss();
       if(resp.status == 200){
---
+++ src/providers/se/APIServer.ts
@@ -0,0 +1,4 @@
+export class APIServer {
+  // request server
+  url : string = 'http://192.168.0.111:3001/file/upload';
+}
